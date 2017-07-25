9c06f238f719bf6f40836e8c1d34a005fa81ef00
johnkang1974 <johnkang@foxmail.com>
2017-07-25 18:59:53 +0800

    modify

---
+++ XJC/src/main/java/name/kangjun/xjc/adapter/ManhuaYizhourenqiAdapter.java
@@ -0,0 +1,10 @@
+package name.kangjun.xjc.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Kangjun on 2017/7/25.
+ */
+
+public class ManhuaYizhourenqiAdapter extends RecyclerView.Adapter {
+}
---
+++ XJC/src/main/java/name/kangjun/xjc/model/YizhourenqiItemBean.java
@@ -0,0 +1,8 @@
+package name.kangjun.xjc.model;
+
+/**
+ * Created by Kangjun on 2017/7/25.
+ */
+
+public class YizhourenqiItemBean {
+}
